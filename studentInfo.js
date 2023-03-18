49c9ed973ebbede0142a61c5efeb2e6b64e4f8b2
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-03-18 23:51:19 +0600

    complete the json file and add read the json file using java script

--- studentInfo.js
+++ studentInfo.js
@@ -1,20 +1,24 @@
 let http = new XMLHttpRequest();
+
 http.open('get','studentInfo.json',true);
+
 http.send();
-http.onload= function(){
-    if (this.readyState == 4 && this.status == 200) {
+
+http.onload = function () {
+    if (this.readyState == 4 && this.status ==200) {
         let products = JSON.parse(this.responseText);
-    
-    let output ="";
-    for( let item of products){
-        output+=`
-        <div class="pannel">
-        <p>'${item.name}'</p>
-        <p>"${item.roll}"</p>
-        <p>"${item.subject}"</p>
-    </div>
-        `;
+
+        let output ="";
+        for (const i of products) {
+             output +=`
+             <div class = "pannel">
+                <p> ${i.name} </p>
+                <p> ${i.roll} </p>
+                <p> ${i.subject} </p>
+             </div>
+
+             `
+        }
+        document.querySelector(".products").innerHTML= output;
     }
-    document.querySelector('.studentInof').innerHTML= output;
-}
 }
