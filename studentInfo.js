71a15c301cc932a7c241ce31b9490cc4d6d69e96
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-03-18 23:46:36 +0600

    add new jsone

--- studentInfo.js
+++ studentInfo.js
@@ -1,18 +1,20 @@
 let http = new XMLHttpRequest();
 http.open('get','studentInfo.json',true);
+http.send();
 http.onload= function(){
-    if (this.readyState == 4 && this.status) {
+    if (this.readyState == 4 && this.status == 200) {
         let products = JSON.parse(this.responseText);
-    }
+    
     let output ="";
-    for( let item of product){
+    for( let item of products){
         output+=`
         <div class="pannel">
-        <p>${item.name}</p>
-        <p>${item.roll}</p>
-        <p>${item.subject}</p>
+        <p>'${item.name}'</p>
+        <p>"${item.roll}"</p>
+        <p>"${item.subject}"</p>
     </div>
         `;
     }
     document.querySelector('.studentInof').innerHTML= output;
+}
 }
